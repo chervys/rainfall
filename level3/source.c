5bf350bf23da7e3557ed83a10150a2e04f4b5275
Charles-Elie Hervy <75300601+chervys@users.noreply.github.com>
2024-04-25 11:11:47 +0200

    Up level3

--- level3/source.c
+++ level3/source.c
@@ -4,8 +4,7 @@ void v(void)
 
     fgets(local_20c, 0x200, stdin);
     printf(local_20c);
-    if (m == 0x40)
-    {
+    if (m == 0x40) {
         fwrite("Wait what?!\n", 1, 0xc, stdout);
         system("/bin/sh");
     }
